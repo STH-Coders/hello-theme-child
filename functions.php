0e95cfabfd94fe2c180a262f54a397c29667eea0
Michael Rehnert (killshot13) <dmreh@outlook.com>
2022-03-23 07:21:01 -0500

    Fix(styles|scripts): minor bug fixes
    Signed-off-by: Michael Rehnert (killshot13) <dmreh@outlook.com>

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@
 // Load child theme css and optional scripts. //
 
 // Hide the Archive Title Prefix -- Credit: Ben Gillbanks //
-function sth_hide_the_archive_title($title)
+function hide_the_archive_title($title)
 {
     // Skip if the site isn't LTR, this is visual, not functional.
     // Should try to work out an elegant solution that works for both directions.
@@ -39,10 +39,10 @@ function sth_hide_the_archive_title($title)
 
     return $title;
 }
-    add_filter('get_the_archive_title', 'sth_hide_the_archive_title');
+    add_filter('get_the_archive_title', 'hide_the_archive_title');
 
 // Disable comments on images //
-function sth_filter_media_comment_status($open, $post_id)
+function filter_media_comment_status($open, $post_id)
 {
     $post = get_post($post_id);
     if ('attachment' == $post->post_type) {
@@ -51,8 +51,7 @@ function sth_filter_media_comment_status($open, $post_id)
 
     return $open;
 }
-add_filter('comments_open', 'sth_filter_media_comment_status', 10, 2);
-
+add_filter('comments_open', 'filter_media_comment_status', 10, 2);
 
 // Custom “search results for:” archive title //
 function archive_callback($title)
@@ -66,12 +65,10 @@ function archive_callback($title)
 }
 add_filter('elementor/utils/get_the_archive_title', 'archive_callback');
 
-
 // Add the menu widgets option //
 if (function_exists('register_sidebar')) {
     register_sidebar();
 }
-
 
 // Enable ACF Custom Fields //
 add_filter('acf/settings/remove_wp_meta_box', '__return_false');
@@ -85,7 +82,7 @@ function hello_elementor_child_enqueue_scripts()
         [
             'hello-elementor-theme-style',
         ],
-        '1.0.0'
+        '1.0.1'
     );
 }
 add_action('wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20);
